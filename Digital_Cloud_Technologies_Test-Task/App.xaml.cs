50b64237216a5d0b74725cf12158fa9df2960af5
Владислав <kirpavladislav584@gmail.com>
2026-05-06 09:39:39 +0300

    Добавьте файлы проекта.

---
+++ Digital_Cloud_Technologies_Test-Task/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Digital_Cloud_Technologies_Test_Task
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
