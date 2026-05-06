6fb6c286612532f870d8ae50d53d2fb5469dac13
Владислав <kirpavladislav584@gmail.com>
2026-05-06 13:03:34 +0300

    Initial data retrieval and display in UI for top 10 coins

---
+++ Digital_Cloud_Technologies_Test-Task/Coin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digital_Cloud_Technologies_Test_Task
+{
+    public class Coin
+    {
+        public string? id { get; set; }
+        public string? symbol { get; set; }
+        public string? name { get; set; }
+        public string? image { get; set; }
+        public double? price_change_percentage_24h { get; set; }
+        public decimal? current_price { get; set; }
+        public double? total_volume { get; set; }
+    }
+}
--- Digital_Cloud_Technologies_Test-Task/MainWindow.xaml.cs
+++ Digital_Cloud_Technologies_Test-Task/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System.Net.Http;
+using System.Text;
+using System.Text.Json;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,6 +21,32 @@ namespace Digital_Cloud_Technologies_Test_Task
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private async void MainButton_Click(object sender, RoutedEventArgs e)
+        {
+            HttpClient coingeckoClient = new HttpClient();
+            string url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1";
+            string key = "CG-Y6FynVZbsykxJXXv72BgLVik";
+            
+            try
+            {
+               
+                coingeckoClient.DefaultRequestHeaders.Add("x-cg-demo-api-key", key);
+                string jsonResult = await coingeckoClient.GetStringAsync(url);
+                var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+                List<Coin>? coins = JsonSerializer.Deserialize<List<Coin>>(jsonResult, options);
+
+                if(coins != null)
+                {
+                    CoinsDataGrid.ItemsSource = coins;
+                }
+
+                
+
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+
         }
     }
 }
